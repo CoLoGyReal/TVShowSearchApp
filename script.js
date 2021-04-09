9445c90943de860999c1bfa9d6a872df160b629f
Alexander Efimov <alexander.efimov.clg@gmail.com>
2021-04-09 16:41:12 +0300

    Add search and connect TVMaze API

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const form = document.querySelector('#searchForm');
+const text = document.querySelector('input');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async function(e) {
     e.preventDefault();
-    const name = form.elements.query.value;
-    console.log(name);
+    const name = text.value;
+    const res = await axios.get(`http://api.tvmaze.com/singlesearch/shows?q=${name}`);
+    console.log(res.data);
+    const img = document.createElement('img');
+    img.src = res.data.image.medium;
+    document.body.append(img);
+
 })
